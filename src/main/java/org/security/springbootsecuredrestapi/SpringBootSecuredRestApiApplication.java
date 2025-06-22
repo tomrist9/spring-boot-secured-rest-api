4624f635b373bbcfe0bb42d51ca4c62d79ea7ffd
Tomris Teymurlu <tomristt9@gmail.com>
2025-06-22 12:42:28 +0400

    Initial commit

---
+++ src/main/java/org/security/springbootsecuredrestapi/SpringBootSecuredRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.security.springbootsecuredrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuredRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecuredRestApiApplication.class, args);
+    }
+
+}
